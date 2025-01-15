1bf4c86ceb327f9f0d5f33d36ac0ecbf24543c7d
easmintule <suraiaeasmin2526@gmail.com>
2025-01-15 12:29:43 +0600

    Input recieving

--- recieving_input.py
+++ recieving_input.py
@@ -1,6 +1,3 @@
 name = input('Whats your name?')
 fav_colr = input('Whats your favourite colour?')
-print(f"{name} + 'likes' + {fav_colr}")
-print(f"{name} + 'likes' + {fav_colr}")
-
-# dsd
+print(name + ' likes ' + fav_colr)
