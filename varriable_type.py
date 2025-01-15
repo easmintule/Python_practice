02ac95f41a2bfa613f427fa9a9675d43ab60f467
easmintule <suraiaeasmin2526@gmail.com>
2025-01-15 12:58:01 +0600

    Varriable type

---
+++ varriable_type.py
@@ -0,0 +1,22 @@
+num1 = 12
+print(type(num1))
+
+num2 = 3.1416
+print(type(num2))
+
+num3 = 2 + 2j
+print(type(num3))
+
+str1 = "Python is fun!"
+print(type(str1))
+
+print(type(True))
+print(type(None))
+
+birth_year = input("My birth year is : ")
+print(type(birth_year))
+current_year = input("current year is : ")
+age = int(current_year) - int(birth_year)
+print(type(age))
+
+print(age)
