2f7b419d4c263f3822635fd17c19c4792a48ec91
easmintule <suraiaeasmin2526@gmail.com>
2025-01-16 21:33:16 +0600

    multiple strings

--- string.py
+++ string.py
@@ -1,2 +1,5 @@
-course = 'Python for beginners'
-print(course) #Python for beginners
+course1 = 'Python for beginners'
+print("course1 :" + course1) #Python for beginners
+
+course2 = "Python for beginners"
+print("course2 :" + course2)
