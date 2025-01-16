ecffa91a2b90e2e2137ed4d0f41bf06584a5910b
easmintule <suraiaeasmin2526@gmail.com>
2025-01-16 21:37:31 +0600

    multiple strings

--- string.py
+++ string.py
@@ -2,4 +2,7 @@ course1 = 'Python for beginners'
 print("course1 :" + course1) #Python for beginners
 
 course2 = "Python for beginners"
-print("course2 :" + course2)
+print("course2 :" + course2) #Python for beginners
+
+course3 = "Python for beginners"
+print("course3 :" + course3[0]) # 0 number index will show that means 'python' index [012345]= 'P'
