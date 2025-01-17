1b168428ff26397c3e78744e285f679e6c51796d
easmintule <suraiaeasmin2526@gmail.com>
2025-01-17 08:51:23 +0600

    Multiple string

--- string.py
+++ string.py
@@ -1,8 +1,23 @@
 course1 = 'Python for beginners'
 print("course1 :" + course1) #Python for beginners
 
-course2 = "Python for beginners"
-print("course2 :" + course2) #Python for beginners
+#course2 = "Python for beginners"
+print("course2 :" + course1) #Python for beginners
 
-course3 = "Python for beginners"
-print("course3 :" + course3[0]) # 0 number index will show that means 'python' index [012345]= 'P'
+#course3 = "Python for beginners"
+print("course3 :" + course1[0]) # 0 number index will show that means 'python' index [012345]= 'P'
+
+#course4 = "Python for beginners"
+print("course4 :" + course1[-1]) # last word 'beginners' = 's' will show
+
+#course5 = "Python for beginners"
+print("course5 :" + course1[0 : 3 ]) #course5 :Pyt; 0 will assume as the start index and lenght of the string will assume as the end index
+
+print("course6 :" + course1[ :  ]) #course5 :[ :  ] in this index basically copy or clone a index.
+                                #output : course6 :Python for beginners
+
+print("course7 :" + course1[ : 7 ]) #course5 :Python
+
+print("course8 :" + course1[7 :  ]) #course5 :for beginners
+
+
