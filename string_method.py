e89b42cc06875ad34a716eec25652e5a58aa69bd
easmintule <suraiaeasmin2526@gmail.com>
2025-01-18 00:01:38 +0600

    String Method

--- string_method.py
+++ string_method.py
@@ -1,3 +1,7 @@
+#Characters of method
 course1 = 'Python for beginners'
 print(course1.upper())#capital form
+print(course1.lower())#small letter formate
 print(course1)#original form
+
+#Find the methods
