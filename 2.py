f4882c33ab15d78c65b0f93a076df7df34c9eb2d
easmintule <suraiaeasmin2526@gmail.com>
2025-11-13 23:24:30 +0600

    practice

---
+++ 2.py
@@ -0,0 +1,9 @@
+a = int(input('give a number : '))
+b = int(input('give a number : '))
+
+sum = a + b
+
+print('sum :' ,a+b)
+print(f'sum : {a+b}') # 3 + 2 = 5 a + b
+
+print(f'sum : {a} + {b} = {sum} ')
