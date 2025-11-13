073fb119c2714ee3efc97c70959f89761565d294
easmintule <suraiaeasmin2526@gmail.com>
2025-11-13 23:35:17 +0600

    practice

---
+++ 3.py
@@ -0,0 +1,12 @@
+a = int(input('give a number : '))
+b = int(input('give a number : '))
+c = int(input('give a number : '))
+
+if a > b:
+    if a > c:
+        print('a is the largest number.')
+
+elif b > c:
+        print('b is the largest number.')
+else:
+    print('c is the largest number.')
